b7f1055b9e432849c3dc08453ab3beba57b0dd4b
ZxStim <thang.victor.luong@gmail.com>
2024-12-09 19:52:24 +0700

    added kit

---
+++ app/provider.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import * as React from 'react';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+} from '@rainbow-me/rainbowkit';
+import {
+  trustWallet,
+  ledgerWallet,
+} from '@rainbow-me/rainbowkit/wallets';
+import {
+  kaia,
+  kairos
+} from 'wagmi/chains';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider, http } from 'wagmi';
+// import according to docs
+
+const { wallets } = getDefaultWallets();
+// initialize and destructure wallets object
+
+const config = getDefaultConfig({
+  appName: 'SIGPASS', // Name your app
+  projectId: process.env.WALLETCONNECT_ID!, // Enter your WalletConnect Project ID here
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [
+    kaia,
+    kairos
+  ],
+  transports: {
+    [kaia.id]: http('https://public-en.node.kaia.io'),
+    [kairos.id]: http('https://public-en-kairos.node.kaia.io'),
+  },
+  ssr: true, // Because it is Nextjs's App router, you need to declare ssr as true
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
