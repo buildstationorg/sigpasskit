0ece0ddd84c57ffb79cc378523cfb983c79c911c
ZxStim <thang.victor.luong@gmail.com>
2025-01-27 15:00:32 +0700

    feat: added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import '@rainbow-me/rainbowkit/styles.css';
 import { Providers } from '@/app/provider';
 import { Toaster } from "@/components/ui/toaster"
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script  
+        defer 
+        src="https://assets.onedollarstats.com/stonks.js" 
+        id="stonks" > 
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
